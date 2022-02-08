ad41e6b1c202fbde4a7f40bbbcbef8ba1f4f5e58
vighakos <vighakos@turr.hu>
2022-02-08 12:50:34 +0100

    csv

---
+++ fajlbe_csv1/fajlbe_csv1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fajlbe_csv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeOlvas("fajl.csv");
+            
+            Console.ReadKey();
+        }
+
+        private static void BeOlvas(string fajlNev)
+        {
+            try
+            {
+                StreamReader be = new StreamReader(fajlNev, Encoding.Default);
+                List<string> adatok = new List<string>();
+                while (!be.EndOfStream)
+                {
+                    string sor = be.ReadLine();
+                    for (int i = 0; i < sor.Split(';').Length; i++)
+                    {
+                        if (sor.Split(';')[i] != "")
+                        {
+                            adatok.Add(sor.Split(';')[i]);
+                        }
+                    }
+                }
+                be.Close();
+                Console.WriteLine($"Az állomány {adatok.Count} elemet olvasott be");
+                Kiir(adatok);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Hiba");
+            }
+        }
+
+        private static void Kiir(List<string> adatok)
+        {
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                Console.WriteLine(adatok[i]);
+            }
+        }
+    }
+}
---
+++ kereses_tablazatban/kereses_tablazatban/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kereses_tablazatban
+{
+    class Program
+    {
+        public static List<string> adatok = new List<string>();
+        static void Main(string[] args)
+        {
+            BeOlvas("adatok.csv");
+            BeKer();
+
+            Console.ReadKey();
+        }
+
+        private static void BeKer()
+        {
+            Console.Write("Adj meg egy sort: ");
+            int sor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Adj meg egy oszlopot: ");
+            int oszlop = Convert.ToInt32(Console.ReadLine());
+
+            if (sor > adatok.Count)
+            {
+                Console.WriteLine("NEM JÓ JKSGLABGKAGKL TÚL SOKKKK");
+            }
+            else if (adatok[0].Split(';').Length < oszlop)
+            {
+                Console.WriteLine("NEM JÓ JKSGLABGKAGKL TÚL SOKKKK");
+            }
+            else
+            {
+                if (adatok[sor - 1].Split(';')[oszlop-1] != "")
+                {
+                    Console.WriteLine($"A kért adat: {adatok[sor - 1].Split(';')[oszlop - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine("Az adott cellában nincs érték");
+                }
+            }
+
+        }
+
+        private static void BeOlvas(string fajlNev)
+        {
+            try
+            {
+                StreamReader be = new StreamReader(fajlNev);
+                while (!be.EndOfStream)
+                {
+                    adatok.Add(be.ReadLine());
+                }
+                be.Close();
+                Console.WriteLine($"Beolvasva {adatok.Count} sor");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Hiba");
+            }
+        }
+    }
+}
