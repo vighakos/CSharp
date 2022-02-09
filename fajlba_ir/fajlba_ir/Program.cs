84572227bc265ec110ab0a20c331d810701ec914
vighakos <vighakos@turr.hu>
2022-02-09 11:55:41 +0100

    kj

---
+++ fajlba_ir/fajlba_ir/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fajlba_ir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
