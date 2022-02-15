17753614c8610ec69f68e1c54289c1a855e8abc7
vighakos <vighakos@turr.hu>
2022-02-15 12:58:05 +0100

    komit

---
+++ osszesites/osszesites/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osszesites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beolvas();
+
+            Console.ReadKey();
+        }
+
+        private static void Beolvas()
+        {
+            try
+            {
+                for (int i = 0; i < Int32.MaxValue; i++)
+                {
+                    if (File.Exists(i + ".txt"))
+                    {
+                        StreamReader be = new StreamReader(i + ".txt");
+                        List<int> adatok = new List<int>();
+                        while (!be.EndOfStream)
+                        {
+                            adatok.Add(Convert.ToInt32(be.ReadLine()));
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                
+            }
+        }
+    }
+}
