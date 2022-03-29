3959b1bc5f6dae86550d451076ff94810bd67693
vighakos <vighakos@turr.hu>
2022-03-29 19:36:00 +0200

    epul

---
+++ OOP_Epul/OOP_Epuletek/Olvas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOP_Epuletek
+{
+    class Olvas
+    {
+        public static List<Epulet> Beolvas(string fileName)
+        {
+            List<Epulet> adatok = new List<Epulet>();
+
+            try
+            {
+                StreamReader be = new StreamReader(fileName);
+                be.ReadLine();
+
+                while (!be.EndOfStream)
+                {
+                    adatok.Add(new Epulet(be.ReadLine().Split(';')));
+                }
+
+                be.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return adatok;
+        }
+
+        public static void Kiir(string fileName, List<Epulet> adatok)
+        {
+            try
+            {
+                StreamWriter ki = new StreamWriter(fileName);
+                for (int i = 0; i < adatok.Count; i++)
+                {
+                    ki.WriteLine(adatok[i].String());
+                }
+                ki.Close();
+                Console.WriteLine("\n\tAdatok fájlba írva");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ OOP_Epul/OOP_Epuletek/Varos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Epuletek
+{
+    class Varos
+    {
+        public string Nev { get; set; }
+        public int Db { get; set; }
+
+        public Varos(string nev)
+        {
+            Nev = nev;
+            Db = 1;
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"\t{Nev} - {Db}");
+        }
+    }
+}
