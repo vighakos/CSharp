f084e839febe0ffc53bdf46cd8b4bb26f967d9c8
vighakos <vighakos@turr.hu>
2022-04-11 12:30:08 +0200

    nyelvvizsga - 4, 5, 6

---
+++ OOP/OOP_Nyelvvizsga/OOP_Nyelvvizsga/Jelentkezo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nyelvvizsga
+{
+    class Jelentkezo
+    {
+        public string Nev, Mobil;
+        public int Szul, Vizsgaszam;
+        public Vizsga Vizsga;
+
+        public Jelentkezo(string s)
+        {
+            Nev = s.Split(';')[0];
+            Mobil = s.Split(';')[1];
+            Szul = Convert.ToInt32(s.Split(';')[2]);
+            Vizsgaszam = Convert.ToInt32(s.Split(';')[3]);
+        }
+
+        public void VizsgaMeghatarozo(List<Vizsga> vizsgak)
+        {
+            for (int i = 0; i < vizsgak.Count; i++)
+            {
+                if (vizsgak[i].Vizsgaszam == this.Vizsgaszam)
+                {
+                    Vizsga = vizsgak[i];
+                }
+            }
+        }
+    }
+}
---
+++ OOP/OOP_Nyelvvizsga/OOP_Nyelvvizsga/Vizsga.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nyelvvizsga
+{
+    class Vizsga
+    {
+        public int Vizsgaszam;
+        public DateTime Datum;
+        public string Nyelv, Szint;
+
+        public Vizsga(string s)
+        {
+            Vizsgaszam = Convert.ToInt32(s.Split(';')[0]);
+            Datum = Convert.ToDateTime(s.Split(';')[1]);
+            Nyelv = s.Split(';')[2];
+            Szint = s.Split(';')[3];
+        }
+    }
+}
