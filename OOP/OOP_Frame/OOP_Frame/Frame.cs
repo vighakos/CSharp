bbc470fca451a33fbf312c582ac8d18a6aa1ed36
vighakos <vighakos@turr.hu>
2022-04-12 14:10:24 +0200

    frame

---
+++ OOP/OOP_Frame/OOP_Frame/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Frame
+{
+    class Frame
+    {
+        public string Nev;
+        public int Health, Shield, TimeToCraft;
+        public double Armor, Energy;
+
+        public Frame(string[] sor)
+        {
+            Nev = sor[0];
+            Health = Convert.ToInt32(sor[1]);
+            Shield = sor[2] == "-" ? 0 : Convert.ToInt32(sor[2]);
+            Energy = sor[3] == "-" ? 0 : Convert.ToDouble(sor[3]);
+            Armor = Convert.ToDouble(sor[4]);
+            TimeToCraft = Convert.ToInt32(sor[5]);
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"\n\tNév: {Nev} " +
+                $"\n\tHP: {Health} " +
+                $"\n\tShield: {Shield} " +
+                $"\n\tEnergy: {Energy} " +
+                $"\n\tArmor: {Armor} " +
+                $"\n\tTTC: {TimeToCraft} óra");
+        }
+    }
+}
---
+++ OOP/OOP_Frame/OOP_Frame/Muvelet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOP_Frame
+{
+    class Muvelet
+    {
+        public static List<Frame> Beolvas(string fileName)
+        {
+            List<Frame> adatok = new List<Frame>();
+
+            try
+            {
+                StreamReader be = new StreamReader(fileName);
+                be.ReadLine();
+
+                while (!be.EndOfStream)
+                {
+                    adatok.Add(new Frame(be.ReadLine().Split(';')));
+                }
+                be.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return adatok;
+        }
+    }
+}
