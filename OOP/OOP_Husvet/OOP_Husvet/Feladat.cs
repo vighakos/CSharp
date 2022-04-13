72af76a8b2f808b8a255320720f5c9776324b555
vighakos <vighakos@turr.hu>
2022-04-13 14:04:51 +0200

    teljesen működik

---
+++ OOP/OOP_Husvet/OOP_Husvet/Feladat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Husvet
+{
+    class Feladat
+    {
+        public static List<Talalat> talalatok = Muvelet.toTalalat(Muvelet.Beolvas("talaltok.txt"));
+        public static List<Talalat> talalatok2 = Muvelet.toTalalat(Muvelet.Beolvas("talaltok.txt"));
+        public static List<PluszTalalat> plusz_talalatok = Muvelet.toPluszTalalat(Muvelet.Beolvas("pluszTalalat.csv"));
+        public static double[] pontok = new double[] { 20, 10.5, 6, 3.5, 1};
+        public Feladat()
+        {
+            _1();
+        }
+
+        private void _1()
+        {
+            int pirosTojasok = talalatok.Sum(x => x.Piros);
+
+        }
+    }
+}
---
+++ OOP/OOP_Husvet/OOP_Husvet/Muvelet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOP_Husvet
+{
+    class Muvelet
+    {
+        public static List<string> Beolvas(string fileName)
+        {
+            List<string> adatok = new List<string>();
+
+            try
+            {
+                StreamReader be = new StreamReader(fileName);
+                be.ReadLine();
+                while (!be.EndOfStream)
+                {
+                    adatok.Add(be.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return adatok;
+        }
+        public static List<Talalat> toTalalat(List<string> data)
+        {
+            List<Talalat> lista = new List<Talalat>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                lista.Add(new Talalat(data[i].Split(';')));
+            }
+            return lista;
+        }
+
+        public static List<PluszTalalat> toPluszTalalat(List<string> data)
+        {
+            List<PluszTalalat> lista = new List<PluszTalalat>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                lista.Add(new PluszTalalat(data[i].Split(';')));
+            }
+            return lista;
+        }
+    }
+}
