66582652d8a50484a607d24e09d2a8acdd7a0c67
vighakos <vighakos@turr.hu>
2022-05-16 13:22:36 +0200

    .

---
+++ OOP/OOP_Decoder/OOP_Decoder/Karakter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Decoder
+{
+    class Karakter
+    {
+        public string Betu;
+        public int[,] KarakterMatrix;
+
+        public Karakter(List<string> sorok)
+        {
+            Betu = sorok[0];
+            KarakterMatrix = new int[7, 4];
+
+            for (int sor = 0; sor < 7; sor++)
+            {
+                for (int oszlop = 0; oszlop < 4; oszlop++)
+                {
+                    KarakterMatrix[sor, oszlop] = Convert.ToInt32(sorok[sor + 1][oszlop].ToString());
+                }
+            }
+        }
+
+        public void Megjelenit()
+        {
+            Console.WriteLine();
+            for (int sor = 0; sor < 7; sor++)
+            {
+                Console.Write("\t");
+                for (int oszlop = 0; oszlop < 4; oszlop++)
+                {
+                    if (KarakterMatrix[sor, oszlop] == 1)
+                        Console.Write("█");
+                    else
+                        Console.Write(" ");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOP/OOP_Decoder/OOP_Decoder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Decoder
+{
+    class Program
+    {
+        static List<Karakter> karakterBank = Fajl.Beolvas("bank.txt");
+        static void Main(string[] args)
+        {
+            _5();
+            _6();
+
+            Console.ReadKey();
+        }
+
+        private static void _6()
+        {
+            string betu = "";
+            do
+            {
+                Console.Write("6. feladat: Adj meg egy angol nagybetűt: ");
+                betu = Console.ReadLine();
+            } while (!BetuEllenor(betu));
+
+            _7(betu);
+            
+        }
+
+        private static void _7(string betu)
+        {
+            bool talalt = false;
+            foreach (Karakter item in karakterBank)
+            {
+                if (item.Betu == betu)
+                {
+                    Console.WriteLine("7. feladat:");
+                    talalt = true;
+                    item.Megjelenit();
+                }
+            }
+
+            if (!talalt)
+            {
+                Console.WriteLine("7. feladat: Nincs ilyen betű");
+            }
+        }
+
+        private static bool BetuEllenor(string betu)
+        {
+            string betuk = "QWERTZUIOPASDFGHJKLYXCVBNM";
+            if (betuk.Contains(betu) && betu.Length == 1) return true;
+            return false;
+        }
+
+        private static void _5()
+        {
+            Console.WriteLine($"5. feladat: karakterek száma: {karakterBank.Count} db");
+        }
+    }
+}
